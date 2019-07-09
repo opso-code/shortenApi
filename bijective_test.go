7d905c62e06c223ac5ed3a4f9cbd9333b3a511a3
opso <945206413@qq.com>
2019-07-09 15:43:48 +0800

    update document

--- bijective_test.go
+++ bijective_test.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"testing"
 )
 
-func TestDecode(t *testing.T) {
+func TestDecodeAndDecode(t *testing.T) {
 	s := "AjaXXe"
 	r := Decode(s)
 	t.Log(r)
@@ -14,34 +13,5 @@ func TestDecode(t *testing.T) {
 	t.Log(s2)
 	if s2 != s {
 		t.Fail()
-	}
-}
-
-func TestShortenV1(t *testing.T) {
-	db, err := sql.Open("mysql", "root:123456@tcp(192.168.33.10:3306)/?charset=utf8")
-	if err != nil {
-		t.Error(err)
-	}
-	row, err := db.Query("SELECT max(`id`) FROM shorten.shorten_v1")
-	if err != nil {
-		t.Error(err)
-	}
-
-	for row.Next() {
-		var v interface{}
-		err = row.Scan(&v)
-		if err != nil {
-			t.Error(err)
-		}
-		t.Logf("结果:%+v\n",v)
-	}
-
-
-	// insert
-	db.Prepare("INSERT IGNORE INTO shorten.shorten_v1 (`sign`,`url`)")
-
-	err = db.Close()
-	if err != nil {
-		t.Error(err)
 	}
 }
--- shorten_v1.sql
+++ shorten_v1.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `shorten`;
 USE shorten;
+# `sign`是长网址经过`md5`后的值，方便以网址查询的时候sql语句好写些，不然`SELECT`的条件查询语句得很乱了,
+# 自增id从300000 开始，所以生成的短码起始为三位数。
 CREATE TABLE IF NOT EXISTS `shorten_v1`
 (
     `id`        bigint(20) unsigned NOT NULL AUTO_INCREMENT,
